0738f49ae84a3820fcc83a9ae26ca2994a6faf43
msgeorgem <marcin.szulc01@gmail.com>
2017-05-27 17:13:21 +0200

    call query without loader

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.android.habittracker;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+
+import static com.example.android.habittracker.Contract.HabitEntry.COLUMN_ITEM_DESCRIPTION;
+import static com.example.android.habittracker.Contract.HabitEntry.COLUMN_ITEM_HABIT;
+import static com.example.android.habittracker.Contract.HabitEntry.COLUMN_ITEM_OCCURENCE;
+import static com.example.android.habittracker.Contract.HabitEntry.COLUMN_ITEM_TIME;
+import static com.example.android.habittracker.Contract.HabitEntry.CONTENT_URI;
+import static com.example.android.habittracker.Contract.HabitEntry._ID;
 
 /**
  * Created by Marcin on 2017-05-21.
@@ -15,11 +22,39 @@ import android.support.v7.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
+    public SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public Cursor getAllHabits() {
+        return db.query(String.valueOf(CONTENT_URI), new String[]{
+                _ID,
+                COLUMN_ITEM_HABIT,
+                COLUMN_ITEM_DESCRIPTION,
+                COLUMN_ITEM_OCCURENCE,
+                COLUMN_ITEM_TIME
+        }, null, null, null, null, null);
+
+    }
+
+    public Cursor getHabit(long rowID) throws SQLException {
+
+        Cursor cursor = db.query(String.valueOf(CONTENT_URI), new String[]{
+                _ID,
+                COLUMN_ITEM_HABIT,
+                COLUMN_ITEM_DESCRIPTION,
+                COLUMN_ITEM_OCCURENCE,
+                COLUMN_ITEM_TIME
+        }, _ID + "=" + rowID, null, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
 
     }
 
@@ -27,31 +62,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Create a ContentValues object, where column names are the keys
         ContentValues values = new ContentValues();
-        values.put(Contract.HabitEntry.COLUMN_ITEM_HABIT, "Dogs");
-        values.put(Contract.HabitEntry.COLUMN_ITEM_DESCRIPTION, "Runnin");
-        values.put(Contract.HabitEntry.COLUMN_ITEM_OCCURENCE, Contract.HabitEntry.DAILY);
-        values.put(Contract.HabitEntry.COLUMN_ITEM_TIME, 7);
+        values.put(COLUMN_ITEM_HABIT, "Dogs");
+        values.put(COLUMN_ITEM_DESCRIPTION, "Runnin");
+        values.put(COLUMN_ITEM_OCCURENCE, Contract.HabitEntry.DAILY);
+        values.put(COLUMN_ITEM_TIME, 7);
 
         // Insert the new row, returning the primary key value of the new row
-        Uri newUri = getContentResolver().insert(Contract.HabitEntry.CONTENT_URI, values);
-    }
-
-
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        String[] projection = {
-                Contract.HabitEntry._ID,
-                Contract.HabitEntry.COLUMN_ITEM_HABIT,
-                Contract.HabitEntry.COLUMN_ITEM_DESCRIPTION,
-
-        };
-
-        // Perform a query using CursorLoader
-        return new CursorLoader(this,    // Parent activity context
-                Contract.HabitEntry.CONTENT_URI, // Provider content URI to query
-                projection,            // The columns to include in the resulting Cursor
-                null,                  // The values for the WHERE clause
-                null,                  // No selection arguments
-                null);                 // Default sort order
+        Uri newUri = getContentResolver().insert(CONTENT_URI, values);
     }
 
 }
